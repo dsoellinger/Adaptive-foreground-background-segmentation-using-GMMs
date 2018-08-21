1b015d690db59f4cde893ac645d004007e7963ae
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2018-08-21 23:23:55 +0200

    Gaussian / Add pdf and partial_fit

--- video_segmentizer/model/gaussian.py
+++ video_segmentizer/model/gaussian.py
@@ -1,5 +1,6 @@
-import numpy as np
 from scipy.spatial.distance import mahalanobis
+from scipy.stats import multivariate_normal
+import numpy as np
 
 
 class Gaussian:
@@ -8,8 +9,21 @@ class Gaussian:
     _covariance = None
 
     def __init__(self, mean, covariance):
-        self._mean = mean
-        self._covariance = covariance
+        self._mean = np.array(mean)
+        self._covariance = np.array(covariance)
 
-    def mahalanobis_distance_between(self, point):
-        return mahalanobis(point, self._mean, self._covariance)
+    def mahalanobis_distance_between(self, x):
+        gaus =  mahalanobis(x, self._mean, self._covariance)
+        return gaus.pdf(x)
+
+    def pdf(self, x):
+        return multivariate_normal(self._mean, self._covariance).pdf(x)
+
+    def get_mean(self):
+        return self._mean
+
+    def partial_fit(self, x, lr=0.75):
+        x = np.array(x)
+        roh = lr * self.pdf(x)
+        self._mean = (1 - roh) * self._mean + roh * x
+        self._covariance = (1-roh) * self._covariance + roh * (x - self._mean).transpose() * (x - self._mean)
--- video_segmentizer/model/gmm.py
+++ video_segmentizer/model/gmm.py
@@ -3,9 +3,11 @@
 class GMM:
 
     _n_clusters = None
+    _gaussians = None
 
     def __init__(self, n_clusters):
         self._n_clusters = n_clusters
 
-    def partial_fit(self, sample):
-        pass
+
+    def partial_fit(self):
+        
